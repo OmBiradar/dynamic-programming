5ab1a1dafe3486835f5bf7b04c9ed9699884ac3b
Om Biradar <ombiradar04@gmail.com>
2024-02-04 01:30:45 +0530

    added features

--- fib.js
+++ fib.js
@@ -2,8 +2,8 @@ const fib = (n) => {
     if (n <= 2) return 1;
     return fib(n - 1) + fib(n - 2);
 };
-
-console.log(fib(6));
+// hello world
+console.log("Fibonacci numbers :")
 console.log(fib(7));
-console.log(fib(8));
+console.log(fib(10));
 console.log(fib(50));
---
+++ new.js
@@ -0,0 +1,3 @@
+console.log("This is a new feature!!!");
+
+// this is a changed lines lol
