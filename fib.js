c3d9b6386dadeb109178467eed282a6a5e2f0cad
Om Biradar <ombiradar04@gmail.com>
2024-02-04 02:17:52 +0530

    used memoization to calculate fibonacci numbers in linear time

--- fib.js
+++ fib.js
@@ -1,9 +1,10 @@
-const fib = (n) => {
+const fib = (n, memo = {}) => {
+    if (n in memo) return memo[n];
     if (n <= 2) return 1;
-    return fib(n - 1) + fib(n - 2);
+    memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+    return memo[n];
 };
-// hello world
-console.log("Fibonacci numbers :")
+
 console.log(fib(7));
 console.log(fib(10));
 console.log(fib(50));
